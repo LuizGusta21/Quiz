790d46bfa88f82febb33448704e0a15e759551d6
Luiz Gustavo <87343462+LuizGusta21@users.noreply.github.com>
2022-12-29 06:25:08 -0300

    Add files via upload

---
+++ src/data/questions.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    question: "O que é Vanilla JavaScript?",
+    options: [
+      "JavaScript puro",
+      "Uma biblioteca JavaScript",
+      "Um framework JavaScript",
+      "Um compilador de JavaScript",
+      "Nsa",
+    ],
+    answer: "JavaScript puro",
+  },
+  {
+    question: "Com qual instrução declaramos uma constante em JavaScript?",
+    options: ["const", "let", "var", "define","Nsa",],
+    answer: "const",
+  },
+  {
+    question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+    options: ["string", "number", "boolean", "float", "Nsa",],
+    answer: "float",
+  },
+  {
+    question: "Qual dos métodos a seguir seleciona um elemento?",
+    options: ["querySelector", "parseInt", "sort", "reduce", "Nsa",],
+    answer: "querySelector",
+  },
+  {
+    question:
+      "Qual destas propriedades da a quantidade de elementos de um array?",
+    options: ["qty", "length", "items", "index", "Nsa",],
+    answer: "length",
+  },
+];
+
+export default data;
